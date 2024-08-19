356000835bb8f096f29fedc1e1206a14da089af7
afetism <aismayilova1215@gmail.com>
2024-08-19 07:50:25 +0400

    comiit

--- ProductManagmentUser/ViewModels/RegisterViewModel.cs
+++ ProductManagmentUser/ViewModels/RegisterViewModel.cs
@@ -1,6 +1,29 @@
 ﻿namespace ProductManagmentUser.ViewModels;
 
     class RegisterViewModel:BaseViewModel
-    {
-       
-    }
+{
+
+
+	private DateTime? selectedDate;
+	public DateTime? SelectedDate
+	{
+		get => selectedDate;
+		set
+		{
+			selectedDate = value;
+			OnPropertyChanged(nameof(SelectedDate));
+		}
+	}
+	private string _password;
+	public string Password
+	{
+		get => _password;
+		set
+		{
+			_password = value;
+			OnPropertyChanged(nameof(Password));
+		}
+	}
+
+
+}
---
+++ ProductManagmentUser/Views/PasswordHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace ProductManagmentUser.Views
+{
+	public static class PasswordHelper
+	{
+		public static readonly DependencyProperty BoundPassword =
+			DependencyProperty.RegisterAttached("BoundPassword", typeof(string), typeof(PasswordHelper), new PropertyMetadata(string.Empty, OnBoundPasswordChanged));
+
+		public static string GetBoundPassword(DependencyObject dp)
+		{
+			return (string)dp.GetValue(BoundPassword);
+		}
+
+		public static void SetBoundPassword(DependencyObject dp, string value)
+		{
+			dp.SetValue(BoundPassword, value);
+		}
+
+		private static void OnBoundPasswordChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var passwordBox = d as PasswordBox;
+
+			if (passwordBox != null)
+			{
+				passwordBox.PasswordChanged -= PasswordChanged;
+
+				if (!string.Equals(passwordBox.Password, e.NewValue as string))
+				{
+					passwordBox.Password = e.NewValue as string;
+				}
+
+				passwordBox.PasswordChanged += PasswordChanged;
+			}
+		}
+
+		private static void PasswordChanged(object sender, RoutedEventArgs e)
+		{
+			var passwordBox = sender as PasswordBox;
+			SetBoundPassword(passwordBox, passwordBox.Password);
+		}
+	}
+}
